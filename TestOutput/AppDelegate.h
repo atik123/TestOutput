f5a68f047f7b3220408537d4b74d2a8ca9ec594e
Atikur Rahman <atikurrahman@Atikurs-MacBook-Pro.local>
2013-11-23 16:05:06 +0600

    Initial Commit

---
+++ TestOutput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestOutput
+//
+//  Created by Atikur Rahman on 11/23/13.
+//  Copyright (c) 2013 Atikur Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
